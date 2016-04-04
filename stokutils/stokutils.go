de7a16869a820b0f55724cd9f2d361a2dcadd34a
Alexandr Razumov <alexandr.razumov@ostrovok.ru>
2016-04-04 21:28:49 +0300

    fix linters for stokutils.go

--- stokutils/stokutils.go
+++ stokutils/stokutils.go
@@ -7,6 +7,13 @@ import (
 	"time"
 )
 
+// IDEAWorkaround is workaround for go-lang-plugin-org/go-lang-idea-plugin#2439.
+//
+// Usage:
+//     func TestMain(m *testing.M) {
+//         m.Run()
+//         IDEAWorkaround()
+//     }
 func IDEAWorkaround() {
 	// workaround for go-lang-plugin-org/go-lang-idea-plugin#2439
 	if len(os.Getenv("IDEAWAIT")) > 0 {
@@ -14,11 +21,13 @@ func IDEAWorkaround() {
 	}
 }
 
+// Fatalist is common interface for testing.T and testing.B
 type Fatalist interface {
 	Fatal(args ...interface{})
 	Error(args ...interface{})
 }
 
+// TempFile returns temporary file and calls t.Fatal if error.
 func TempFile(t Fatalist) *os.File {
 	f, err := ioutil.TempFile("", "")
 	if err != nil {
@@ -27,6 +36,8 @@ func TempFile(t Fatalist) *os.File {
 	return f
 }
 
+// ClearTempFile closes and removes given file and calls t.Error
+// on Close error and t.Fatal on Remove error.
 func ClearTempFile(f *os.File, t Fatalist) {
 	name := f.Name()
 	if err := f.Close(); err != nil {
@@ -44,16 +55,19 @@ type MemoryBackend struct {
 	Reader   bytes.Reader
 }
 
+// Truncate underlying buffer
 func (m MemoryBackend) Truncate(size int64) error {
 	m.Buff.Grow(int(size))
 	m.Reader = *bytes.NewReader(m.Buff.Bytes())
 	return nil
 }
 
+// ReadAt from reader
 func (m MemoryBackend) ReadAt(b []byte, off int64) (int, error) {
 	return m.Reader.ReadAt(b, off)
 }
 
+// WriteAt to underlying buffer and rewrite Reader
 func (m *MemoryBackend) WriteAt(b []byte, off int64) (int, error) {
 	n, err := m.Buff.Write(b)
 	if err != nil {
@@ -63,30 +77,37 @@ func (m *MemoryBackend) WriteAt(b []byte, off int64) (int, error) {
 	return n, nil
 }
 
+// Stat returns self (MemoryBackend) and nil error.
 func (m MemoryBackend) Stat() (os.FileInfo, error) {
 	return m, nil
 }
 
+// Name returns FileName field value.
 func (m MemoryBackend) Name() string {
 	return m.FileName
 }
 
+// Size returns length of underlying buffer.
 func (m MemoryBackend) Size() int64 {
 	return int64(m.Buff.Len())
 }
 
+// Mode is always 0666
 func (m MemoryBackend) Mode() os.FileMode {
 	return os.FileMode(0666)
 }
 
+// IsDir is always false.
 func (m MemoryBackend) IsDir() bool {
 	return false
 }
 
+// Sys returns underlying buffer.
 func (m MemoryBackend) Sys() interface{} {
 	return m.Buff
 }
 
+// ModTime is always zero time.
 func (m MemoryBackend) ModTime() time.Time {
 	return time.Time{}
 }
