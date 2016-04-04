1a16016cc0351b52dc8b4269edfeb971531a4672
Alexandr Razumov <alexandr.razumov@ostrovok.ru>
2016-04-04 15:56:49 +0300

    refactor tests

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -53,93 +53,71 @@ func TestVolume_ReadFile(t *testing.T) {
 	}
 }
 
+func benchmarkVolumeReadFile(b *testing.B, d []byte) {
+	iB := tempFile(b)
+	defer clearTempFile(iB, b)
+	bB := tempFile(b)
+	defer clearTempFile(bB, b)
+
+	v := Volume{
+		Index: Index{
+			Backend: iB,
+		},
+		Bulk: Bulk{
+			Backend: bB,
+		},
+	}
+
+	h := Header{
+		ID:        0,
+		Size:      len(d),
+		Offset:    0,
+		Timestamp: time.Now().Unix(),
+	}
+	l := h.Link()
+
+	buff := AcquireByteBuffer()
+	defer ReleaseByteBuffer(buff)
+	if err := v.Index.WriteBuff(l, buff.B[:LinkStructureSize]); err != nil {
+		b.Error(err)
+	}
+	if err := v.Bulk.Write(h, d); err != nil {
+		b.Error(err)
+	}
+
+	callback := func(rh Header, rd []byte) error {
+		return nil
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := v.ReadFile(h.ID, callback); err != nil {
+			b.Error(err)
+		}
+	}
+}
+
 func BenchmarkVolume_ReadFile70b(b *testing.B) {
-	iB := tempFile(b)
-	defer clearTempFile(iB, b)
-	bB := tempFile(b)
-	defer clearTempFile(bB, b)
-
-	v := Volume{
-		Index: Index{
-			Backend: iB,
-		},
-		Bulk: Bulk{
-			Backend: bB,
-		},
-	}
-
 	d := []byte("Data data data data data data data data data data data data data data!")
-	h := Header{
-		ID:        0,
-		Size:      len(d),
-		Offset:    0,
-		Timestamp: time.Now().Unix(),
-	}
-	l := h.Link()
-
-	buff := AcquireByteBuffer()
-	defer ReleaseByteBuffer(buff)
-	if err := v.Index.WriteBuff(l, buff.B[:LinkStructureSize]); err != nil {
-		b.Error(err)
-	}
-	if err := v.Bulk.Write(h, d); err != nil {
-		b.Error(err)
-	}
-
-	callback := func(rh Header, rd []byte) error {
-		return nil
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		if err := v.ReadFile(h.ID, callback); err != nil {
-			b.Error(err)
-		}
-	}
+	benchmarkVolumeReadFile(b, d)
 }
 
-func BenchmarkVolume_ReadFile500kb(b *testing.B) {
-	iB := tempFile(b)
-	defer clearTempFile(iB, b)
-	bB := tempFile(b)
-	defer clearTempFile(bB, b)
-
-	v := Volume{
-		Index: Index{
-			Backend: iB,
-		},
-		Bulk: Bulk{
-			Backend: bB,
-		},
-	}
-
-	d := make([]byte, defaultByteBufferSize+1024)
+func BenchmarkVolume_ReadFile1x(b *testing.B) {
+	d := make([]byte, defaultByteBufferSize+1)
 	copy(d, []byte("data"))
-	h := Header{
-		ID:        0,
-		Size:      len(d),
-		Offset:    0,
-		Timestamp: time.Now().Unix(),
-	}
-	l := h.Link()
-
-	buff := AcquireByteBuffer()
-	defer ReleaseByteBuffer(buff)
-	if err := v.Index.WriteBuff(l, buff.B[:LinkStructureSize]); err != nil {
-		b.Error(err)
-	}
-	if err := v.Bulk.Write(h, d); err != nil {
-		b.Error(err)
-	}
-
-	callback := func(rh Header, rd []byte) error {
-		return nil
-	}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		if err := v.ReadFile(h.ID, callback); err != nil {
-			b.Error(err)
-		}
-	}
+	benchmarkVolumeReadFile(b, d)
 }
+
+func BenchmarkVolume_ReadFile2x(b *testing.B) {
+	d := make([]byte, defaultByteBufferSize*2)
+	copy(d, []byte("data"))
+	benchmarkVolumeReadFile(b, d)
+}
+
+
+func BenchmarkVolume_ReadFileHalf(b *testing.B) {
+	d := make([]byte, defaultByteBufferSize/2)
+	copy(d, []byte("data"))
+	benchmarkVolumeReadFile(b, d)
+}
+
